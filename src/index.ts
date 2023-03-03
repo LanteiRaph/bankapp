2e1afce9b204a56d5efcf574964257df7fe9beff
lanteiraph <santamulantei8@gmail.com>
2023-03-03 22:13:55 +0300

    feat: add mongo database

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
-import bodyParser from 'body-parser';
-import express from 'express'
-import expressLayouts  from 'express-ejs-layouts'
-import path from 'path';
-import accountRoute from './server/routes/account.routes';
-const app = express()
+import bodyParser from "body-parser";
+import express from "express";
+import expressLayouts from "express-ejs-layouts";
+import mongoose from "mongoose";
+import path from "path";
+import accountRoute from "./server/routes/account.routes";
+const app = express();
+const uri =
+  "mongodb+srv://lantei:lantei95@cluster0.aybaugd.mongodb.net/?retryWrites=true&w=majority";
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected To database"))
+  .catch((err) => console.log(err));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.set('layout', path.join(__dirname, '/views/pages/layouts/main.ejs'))
+app.set("layout", path.join(__dirname, "/views/pages/layouts/main.ejs"));
 
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static('public'));
-app.use(expressLayouts)
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static("public"));
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
-app.use('/',accountRoute);
+app.use("/", accountRoute);
 
-app.listen(5550,() => {
-    console.log(`Web Server running on port 5550`)
-})
+app.listen(5550, () => {
+  console.log(`Web Server running on port 5550`);
+});
