84d9406240e4fe1b7ae4246f75b67f64c36ebe39
lanteiraph <santamulantei8@gmail.com>
2023-03-07 19:43:34 +0300

    feat: add feature cards to index page

--- src/server/routes/account.routes.ts
+++ src/server/routes/account.routes.ts
@@ -4,16 +4,7 @@ import { createAccount } from "../controllers/accounts.controller";
 
 
 // Satatic pages
-router.get("/create-account", (req, res) => {
-  res.render("pages/create-account");
-});
 
-router.get("/transfer", (req, res) => {
-  res.render("pages/transfer");
-});
-router.get('/deposit', (req, res) => {
-  res.render('pages/deposit')
-})
 
 
 //Api Routes for data handling
--- src/server/routes/app.routes.ts
+++ src/server/routes/app.routes.ts
@@ -3,10 +3,26 @@ import {Router} from 'express';
 const router= Router()
 
 router.get("/", (req, res) => {
-    res.render("pages/index");
+    res.render("pages/index", {features: [{name: 'Deposit', url: '/deposit'}]});
   });
 router.get('/about', (req, res) => {
     res.render('pages/about')
+})
+
+router.get("/create-account", (req, res) => {
+  res.render("pages/create-account");
+});
+
+router.get("/transfer", (req, res) => {
+  res.render("pages/transfer");
+});
+
+router.get('/deposit', (req, res) => {
+  res.render('pages/deposit')
+})
+
+router.get('/contact', (req, res) => {
+  res.render('pages/contact')
 })
 
 
